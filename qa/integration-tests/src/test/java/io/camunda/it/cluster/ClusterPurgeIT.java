10139278b49e5c2a850c62e68e96ad09038153df
Simon Sprünker <simon@spruenker.de>
2025-01-15 10:39:27 +0100

    test: add more e2e tests

--- qa/integration-tests/src/test/java/io/camunda/it/cluster/ClusterPurgeIT.java
+++ qa/integration-tests/src/test/java/io/camunda/it/cluster/ClusterPurgeIT.java
@@ -8,7 +8,13 @@
 package io.camunda.it.cluster;
 
 import io.camunda.client.CamundaClient;
+import io.camunda.client.api.command.ProblemException;
+import io.camunda.client.api.search.response.SearchQueryResponse;
+import io.camunda.client.api.search.response.UserTask;
+import io.camunda.zeebe.management.cluster.PlannedOperationsResponse;
 import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import io.camunda.zeebe.model.bpmn.builder.AbstractUserTaskBuilder;
 import io.camunda.zeebe.qa.util.actuator.ClusterActuator;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
@@ -16,6 +22,7 @@ import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.camunda.zeebe.qa.util.topology.ClusterActuatorAssert;
 import java.time.Duration;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import org.assertj.core.api.Assertions;
 import org.awaitility.Awaitility;
@@ -36,17 +43,167 @@ public class ClusterPurgeIT {
 
   @Test
   void shouldPurgeProcessDefinitions() {
+    // GIVEN
     final var processModel =
         Bpmn.createExecutableProcess("test-process").startEvent().endEvent().done();
-    final var clientBuilder = cluster.newClientBuilder();
-    client = clientBuilder.build();
-    final var result =
+    client = cluster.newClientBuilder().build();
+    final var processDefinitionKey = deployProcessModel(processModel);
+    final var actuator = ClusterActuator.of(cluster.availableGateway());
+
+    // WHEN
+    final var planChangeResponse = actuator.purge(false);
+
+    // THEN
+    assertThatChangesAreApplied(planChangeResponse);
+
+    assertThatEntityNotFound(client.newProcessDefinitionGetRequest(processDefinitionKey).send());
+  }
+
+  @Test
+  void shouldPurgeProcessInstances() {
+    // GIVEN
+    client = cluster.newClientBuilder().build();
+    final var processModel =
+        Bpmn.createExecutableProcess("test-process").startEvent().endEvent().done();
+    final var processDefinitionKey = deployProcessModel(processModel);
+    final var processInstanceKey = startProcess(processDefinitionKey);
+
+    final var actuator = ClusterActuator.of(cluster.availableGateway());
+
+    // WHEN
+    final var planChangeResponse = actuator.purge(false);
+
+    // THEN
+    assertThatChangesAreApplied(planChangeResponse);
+
+    assertThatEntityNotFound(client.newProcessInstanceGetRequest(processInstanceKey).send());
+  }
+
+  @Test
+  void shouldPurgeServiceTask() {
+    // GIVEN
+    client = cluster.newClientBuilder().build();
+    final var processModel =
+        Bpmn.createExecutableProcess("test-process")
+            .startEvent()
+            .serviceTask("service-task-1")
+            .zeebeJobType("test")
+            .endEvent()
+            .done();
+    final var processDefinitionKey = deployProcessModel(processModel);
+    final var processInstanceKey = startProcess(processDefinitionKey);
+
+    // TODO
+    final var activateJobs =
+        client.newActivateJobsCommand().jobType("test").maxJobsToActivate(1).send().join();
+    activateJobs.getJobs().getFirst().getKey();
+
+    //    final var result = client.newUserTaskQuery().send().join();
+
+    final var actuator = ClusterActuator.of(cluster.availableGateway());
+
+    // WHEN
+    final var planChangeResponse = actuator.purge(false);
+
+    // THEN
+    assertThatChangesAreApplied(planChangeResponse);
+
+    // TODO Should fail
+    client.newCompleteCommand(activateJobs.getJobs().getFirst());
+  }
+
+  @Test
+  void shouldPurgeUserTask() throws InterruptedException {
+    // GIVEN
+    client = cluster.newClientBuilder().build();
+    final var processModel =
+        Bpmn.createExecutableProcess("test-process")
+            .startEvent()
+            .userTask("user-task-1", AbstractUserTaskBuilder::zeebeUserTask)
+            .endEvent()
+            .done();
+    final var processDefinitionKey = deployProcessModel(processModel);
+    final var processInstanceKey = startProcess(processDefinitionKey);
+
+    Awaitility.await("until user task is active")
+        .ignoreExceptions()
+        .atMost(Duration.ofSeconds(20))
+        .untilAsserted(
+            () -> {
+              final Future<SearchQueryResponse<UserTask>> userTaskFuture =
+                  client.newUserTaskQuery().send();
+              Assertions.assertThat(userTaskFuture)
+                  .succeedsWithin(Duration.ofSeconds(10))
+                  .extracting(SearchQueryResponse::items)
+                  .satisfies(items -> Assertions.assertThat(items).hasSize(1));
+            });
+
+    final var actuator = ClusterActuator.of(cluster.availableGateway());
+
+    // WHEN
+    final var planChangeResponse = actuator.purge(false);
+
+    // THEN
+    assertThatChangesAreApplied(planChangeResponse);
+
+    final Future<SearchQueryResponse<UserTask>> userTaskFuture = client.newUserTaskQuery().send();
+    Assertions.assertThat(userTaskFuture)
+        .succeedsWithin(Duration.ofSeconds(10))
+        .extracting(SearchQueryResponse::items)
+        .satisfies(items -> Assertions.assertThat(items).isEmpty());
+  }
+
+  private long startProcess(final long processDefinitionKey) {
+    final var processInstanceKey =
+        client
+            .newCreateInstanceCommand()
+            .processDefinitionKey(processDefinitionKey)
+            .send()
+            .join()
+            .getProcessInstanceKey();
+    Awaitility.await("until process instance is created")
+        .atMost(Duration.ofSeconds(20))
+        .untilAsserted(
+            () -> {
+              final Future<?> futureRequest =
+                  client.newProcessInstanceGetRequest(processInstanceKey).send();
+              Assertions.assertThat(futureRequest).succeedsWithin(Duration.ofSeconds(10));
+            });
+    return processInstanceKey;
+  }
+
+  private void assertThatEntityNotFound(final Future<?> future) {
+    Assertions.assertThat(future)
+        .failsWithin(Duration.ofSeconds(10))
+        .withThrowableOfType(ExecutionException.class)
+        .withCauseInstanceOf(ProblemException.class)
+        .withMessageContaining("NOT_FOUND");
+  }
+
+  private void assertThatChangesAreApplied(final PlannedOperationsResponse planChangeResponse) {
+    Assertions.assertThat(planChangeResponse.getPlannedChanges()).isNotEmpty();
+
+    Awaitility.await("until cluster purge completes")
+        .untilAsserted(
+            () -> {
+              ClusterActuatorAssert.assertThat(cluster).hasCompletedChanges(planChangeResponse);
+            });
+  }
+
+  /**
+   * Deploys a process model and waits until it is accessible via the API.
+   *
+   * @return the process definition key
+   */
+  private long deployProcessModel(final BpmnModelInstance processModel) {
+    final var deploymentEvent =
         client
             .newDeployResourceCommand()
             .addProcessModel(processModel, "test-process.bpmn")
             .send()
             .join();
-    final var processDefinitionKey = result.getProcesses().getFirst().getProcessDefinitionKey();
+    final var processDefinitionKey =
+        deploymentEvent.getProcesses().getFirst().getProcessDefinitionKey();
     Awaitility.await("until process model is accessible via API")
         .atMost(Duration.ofSeconds(20))
         .untilAsserted(
@@ -55,19 +212,6 @@ public class ClusterPurgeIT {
                   client.newProcessDefinitionGetRequest(processDefinitionKey).send();
               Assertions.assertThat(futureRequest).succeedsWithin(Duration.ofSeconds(10));
             });
-    final var actuator = ClusterActuator.of(cluster.availableGateway());
-    final var planChangeResponse = actuator.purge(false);
-
-    Assertions.assertThat(planChangeResponse.getPlannedChanges()).isNotEmpty();
-
-    Awaitility.await("until cluster purge completes")
-        .untilAsserted(
-            () -> {
-              ClusterActuatorAssert.assertThat(cluster).hasCompletedChanges(planChangeResponse);
-            });
-
-    final Future<?> futureRequest =
-        client.newProcessDefinitionGetRequest(processDefinitionKey).send();
-    Assertions.assertThat(futureRequest).failsWithin(Duration.ofSeconds(10));
+    return processDefinitionKey;
   }
 }
